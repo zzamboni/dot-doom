f419b578113d2a5ef0df74510ec6653539c0ed7f
Diego Zamboni <diego@zzamboni.org>
2020-08-20 12:08:01 +0200

    Renamed :ui pretty-code to :ui ligatures and :tools macos to :os macos

--- init.el
+++ init.el
@@ -39,7 +39,7 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       pretty-code       ; ligatures or substitute text with pretty symbols
+       ligatures       ; ligatures or substitute text with pretty symbols
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
@@ -92,7 +92,6 @@
        gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        lsp
-       macos             ; MacOS-specific commands
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -106,6 +105,7 @@
 
        :os
        ;;tty               ; improve the terminal Emacs experience
+       macos             ; MacOS-specific commands
 
        :lang
        ;;agda              ; types of types of types of types...
