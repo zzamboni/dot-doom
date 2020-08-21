a55e34bc8f3657fbb545ff2c3a156dfd19a4207f
Diego Zamboni <diego@zzamboni.org>
2020-08-21 21:12:28 +0200

    Enabled pass module

--- init.el
+++ init.el
@@ -94,7 +94,7 @@
        lsp
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
-       ;;pass              ; password manager for nerds
+       pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
