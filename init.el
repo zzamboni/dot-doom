e3bb921000d3a3a1bca5ecb36d752fc287200399
Diego Zamboni <diego@zzamboni.org>
2020-08-25 23:40:10 +0200

    Added :lang org +present option

--- init.el
+++ init.el
@@ -145,7 +145,7 @@
        ;;nim                                    ; python + lisp at the speed of c
        ;;nix                                    ; I hereby declare "nix geht mehr!"
        ;;ocaml                                  ; an objective camel
-       (org +pretty +journal +hugo +dragndrop +roam +pandoc)  ; organize your plain life in plain text
+       (org +pretty +journal +hugo +dragndrop +roam +pandoc +present)  ; organize your plain life in plain text
        ;;php                                    ; perl's insecure younger brother
        plantuml                                 ; diagrams for confusing people more
        ;;purescript                             ; javascript, but functional
