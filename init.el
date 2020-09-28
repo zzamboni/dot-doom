7da575e6d46eedd083fc7555dce1be1100a7426e
Diego Zamboni <diego@zzamboni.org>
2020-09-28 11:15:37 +0200

    Disabled vc-gutter and word-wrap modules

--- init.el
+++ init.el
@@ -43,7 +43,7 @@
        ;;tabs                                   ; a tab bar for Emacs
        ;;treemacs                               ; a project drawer, like neotree but cooler
        ;;unicode                                ; extended unicode support for various languages
-       vc-gutter                                ; vcs diff in the fringe
+       ;;vc-gutter                                ; vcs diff in the fringe
        vi-tilde-fringe                          ; fringe tildes to mark beyond EOB
        window-select                          ; visually switch windows
        workspaces                               ; tab emulation, persistence & separate workspaces
@@ -61,7 +61,7 @@
        ;;parinfer                               ; turn lisp into python, sort of
        ;;rotate-text                            ; cycle region at point between text candidates
        snippets                                 ; my elves. They type so I don't have to
-       word-wrap                              ; soft wrapping with language-aware indent
+       ;;word-wrap                              ; soft wrapping with language-aware indent
 
        :emacs
        dired                                    ; making dired pretty [functional]
