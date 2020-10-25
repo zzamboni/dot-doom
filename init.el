8f019f9d390c72a903cc4c75466f16090a330641
Diego Zamboni <diego@zzamboni.org>
2020-10-25 21:26:38 +0100

    Enabled common-lisp package

--- init.el
+++ init.el
@@ -120,7 +120,7 @@
  ;;agda                ; types of types of types of types...
  ;;cc                  ; C/C++/Obj-C madness
  ;;clojure             ; java with a lisp
- ;;common-lisp         ; if you've seen one lisp, you've seen them all
+ common-lisp           ; if you've seen one lisp, you've seen them all
  ;;coq                 ; proofs-as-programs
  ;;crystal             ; ruby at the speed of c
  ;;csharp              ; unity, .NET, and mono shenanigans
