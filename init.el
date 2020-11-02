95e22d523610c5ed8b988640e8cace65a65ab678
Diego Zamboni <diego@zzamboni.org>
2020-11-02 17:43:14 +0100

    Updated doc and disabled pdf module

--- init.el
+++ init.el
@@ -104,7 +104,7 @@
  (magit +forge)        ; a git porcelain for Emacs
  ;;make                ; run make tasks from Emacs
  pass                  ; password manager for nerds
- pdf                   ; pdf enhancements
+ ;;pdf                 ; pdf enhancements
  ;;prodigy             ; FIXME managing external services & code builders
  ;;rgb                 ; creating color strings
  ;;taskrunner          ; taskrunner for all your projects
