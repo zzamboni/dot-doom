98e3694d1cab3d6a1db6a92e049784bbc6ef9a55
Diego Zamboni <diego@zzamboni.org>
2020-12-16 09:18:16 +0100

    Some test code for modifying md export

--- init.el
+++ init.el
@@ -48,7 +48,7 @@
  nav-flash             ; blink cursor line after big motions
  ;;neotree             ; a project drawer, like NERDTree for vim
  ophints               ; highlight the region an operation acts on
- ;;(popup +defaults)   ; tame sudden yet inevitable temporary windows
+ (popup +defaults)   ; tame sudden yet inevitable temporary windows
  ;;tabs                ; a tab bar for Emacs
  ;;treemacs            ; a project drawer, like neotree but cooler
  ;;unicode             ; extended unicode support for various languages
