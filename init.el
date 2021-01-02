c072d65739dffcf1775efa610e6ffcac466df6c1
Diego Zamboni <diego@zzamboni.org>
2021-01-02 22:02:48 +0100

    Added org -dragndrop

--- init.el
+++ init.el
@@ -153,7 +153,7 @@
  ;;nim                 ; python + lisp at the speed of c
  ;;nix                 ; I hereby declare "nix geht mehr!"
  ;;ocaml               ; an objective camel
- (org +pretty +journal
+ (org +pretty +journal -dragndrop
       +hugo +roam +pandoc
       +present)        ; organize your plain life in plain text
  ;;php                 ; perl's insecure younger brother
