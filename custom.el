bff73650fe76c2040f5231da4309a88657b557bc
Diego Zamboni <diego@zzamboni.org>
2021-01-15 17:40:41 +0100

    New safe variable for org-re-reveal-root

--- custom.el
+++ custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((eval add-hook 'after-save-hook
+   '((org-re-reveal-root . "revealjs/")
+     (eval add-hook 'after-save-hook
            (lambda nil
              (org-export-to-file 'awesomecv "zamboni-vita.tex"))
            :append :local)
