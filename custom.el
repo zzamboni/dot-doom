b5b81e9e266e1352df29cf36bcf3f1e56c24d630
Diego Zamboni <diego@zzamboni.org>
2023-05-04 16:12:22 +0200

    Some new safe local variables declared

--- custom.el
+++ custom.el
@@ -5,16 +5,20 @@
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
    '((eval add-hook 'after-save-hook
-           (lambda nil
-             (org-export-to-file 'awesomecv "zamboni-resume.tex"))
-           :append :local)
+      (lambda nil
+        (org-export-to-file 'awesomecv "src/zamboni-vita.tex"))
+      :append :local)
+     (eval add-hook 'after-save-hook
+      (lambda nil
+        (org-export-to-file 'awesomecv "zamboni-resume.tex"))
+      :append :local)
      (org-re-reveal-root . "revealjs/")
      (eval add-hook 'after-save-hook
-           (lambda nil
-             (org-export-to-file 'awesomecv "zamboni-vita.tex"))
-           :append :local)
+      (lambda nil
+        (org-export-to-file 'awesomecv "zamboni-vita.tex"))
+      :append :local)
      (eval add-hook! after-save :append :local
-           (zz/refresh-reveal-prez)))))
+      (zz/refresh-reveal-prez)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
