127e28ac0752ea9fb31286a1d7dd2c81b91def29
Diego Zamboni <diego@zzamboni.org>
2025-10-18 00:42:06 +0200

    Removed spurious custom variable

--- custom.el
+++ custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/.config/elvish/rc.org"))
  '(safe-local-variable-values
    '((eval add-hook 'after-save-hook
       (lambda nil (org-export-to-file 'awesomecv "src/zamboni-vita.tex"))
